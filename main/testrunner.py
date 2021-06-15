afd1f18a49ac961b4e88204089ab436ca2cc34ab
Dillip Nayak <dillipnayak@Dillips-MBP.hitronhub.home>
2021-06-15 17:13:35 -0400

    added run_tag method

---
+++ main/testrunner.py
@@ -0,0 +1,75 @@
+import sys
+import os
+import subprocess
+from behave.__main__ import main as behave_main
+from utils import util
+
+from behave import runner_util as ru
+
+
+
+def run_scenario(scenario):
+    config=util.read_yaml()
+    report_folder=config.get('execution').get('report_folder')
+    report_format=config.get('execution').get('report_format')
+   
+    command = f"{os.getcwd()}/features/sprint1 -f {report_format} -o {report_folder} -n '{scenario}'"
+    print(command)
+    behave_main(command)
+
+def run_tag(tag):
+    config=util.read_yaml()
+    report_folder=config.get('execution').get('report_folder')
+    report_format=config.get('execution').get('report_format')
+
+    command = f"{os.getcwd()}/features/sprint1 -f {report_format} -o {report_folder} -t '{tag}'"
+    print(command)
+    behave_main(command)
+
+
+
+def runtest(args):
+    try:
+        print(f"Arguments : {(args)}")
+        exmod=args[1]
+        print("twinkle", args)
+        if (len(args)==2 and (exmod == 's')):
+            print('Please enter the scenario name as the second parameter')
+       
+        elif len(args)>2 and exmod=='s':
+            scenario=args[2]
+            print("scenariomanpreet")
+            run_scenario(scenario)
+        
+        elif len(args)>2 and exmod=='t':
+            tag=args[2]
+            print("djwalebabu", tag )
+            run_tag(tag)
+    except:
+        print("Error occured by running this suite")
+
+
+
+
+def main():
+    
+    print(f"Arguments : {(sys.argv)}")
+    exmod=sys.argv[1]
+    if (len(sys.argv)==2 and (exmod == 's')):
+        print('Please enter the scenario name as the second parameter')
+        
+
+
+    
+    elif len(sys.argv)>2 :
+        if exmod=='s':
+            run_scenario(sys.argv[2])
+        elif exmod=='t':
+            run_tag(sys.argv[2])
+    
+    # for f in sys.argv:
+    #     print(f.index+" "+f)
+    #behave_main("features/sprint1" )
+
+if __name__ == "__main__":
+    main()
